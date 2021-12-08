3184cda85bc0b8792de2f35fdcf89d83010cfb9e
Marcos Mazini <matias.faefid@hotmail.com>
2021-12-08 11:08:01 -0300

    Template site

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '97fbfe11619a68931aeb276f55e29ac0d22d0b82',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '97fbfe11619a68931aeb276f55e29ac0d22d0b82',
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
